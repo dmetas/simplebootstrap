489f3a0a914ed3c902bcb1dd17437bc834b979dd
Dylan Metas <dylanmetas@gmail.com>
2017-08-31 16:24:11 -0700

    add delete functin to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private def post_params
